73cc42c55ff5e3020bedebefcc5d58c794fcae07
kkaapp11 <102500345+kkaapp11@users.noreply.github.com>
2022-05-31 15:56:06 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    // swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 100,
+    loop:true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
